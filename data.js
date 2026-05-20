3935526c8502e93627862c13167d4fc9830ddb7c
ElocNarf <elocnarf@gmail.com>
2026-05-20 14:23:10 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,28 @@
+[
+  {
+    "year": 2024,
+    "group": null,
+    "state": "Massachusetts",
+    "city": "Boston",
+    "location": "Boston",
+    "team": "Celtics",
+    "win": 1,
+    "lose": 0,
+    "sport": "Basketball",
+    "lat": 42.3601,
+    "lng": -71.0589
+  },
+  {
+    "year": 2023,
+    "group": null,
+    "state": "Colorado",
+    "city": "Denver",
+    "location": "Denver",
+    "team": "Nuggets",
+    "win": 1,
+    "lose": 0,
+    "sport": "Basketball",
+    "lat": 39.7392,
+    "lng": -104.9903
+  }
+]
